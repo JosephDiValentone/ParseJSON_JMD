4bdd2322a6709643996d2d8d30aede275ac8233a
Joseph Divalentone <jmd16a@dmc-207-16.local>
2019-03-08 08:17:41 -0600

    Initial Commit

---
+++ ParseJSON_JMDTests/ParseJSON_JMDTests.swift
@@ -0,0 +1,34 @@
+//
+//  ParseJSON_JMDTests.swift
+//  ParseJSON_JMDTests
+//
+//  Created by Joseph Divalentone on 3/8/19.
+//  Copyright © 2019 Joseph Divalentone. All rights reserved.
+//
+
+import XCTest
+@testable import ParseJSON_JMD
+
+class ParseJSON_JMDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
