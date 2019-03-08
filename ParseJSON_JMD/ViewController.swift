f3ce465a41bf8acbbe563edbb27bd8b124f8881a
Joseph Divalentone <jmd16a@dmc-207-16.local>
2019-03-08 08:33:10 -0600

    EC for IOS

--- ParseJSON_JMD/ViewController.swift
+++ ParseJSON_JMD/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    class Country:  Decodable{
+        let name: String
+        let capital: String
+        let region: String
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do {
+                var countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for  country in countries{
+                    print(country.name + ":" + country.capital)
+                }
+            }
+            catch{
+                print("we got an error")
+                
+            }
+            
+        }.resume()
     }
 
 
